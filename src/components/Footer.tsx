0dc61b115b578f73a181ff342e7e7000cdb8ae51
Karan Sekhri <git@ksekhri.com>
2023-03-21 22:11:05 -0700

    Add link to source

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => (
     <footer>
         <div className="mb-3 text-center">
             © 2023 <a href="https://ksekhri.com">Karan Sekhri</a> —{' '}
-            <a href="https://github.com/ksekhri">Source</a>
+            <a href="https://github.com/ksekhri/race-layout">Source</a>
         </div>
     </footer>
 )
